aabacb112ce2f019f62840b922917c0629e2346b
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-05-14 15:30:54 -0500

    files sourced in and ready to go

---
+++ client.js
@@ -0,0 +1,11 @@
+// is js loaded in console?
+console.log('js loaded');
+
+// DOM ready
+$( document ).ready( readyNow );
+
+
+function readyNow() {
+    //code triggered here is safe to manipulate DOM
+    console.log('DOM IS READY, jquery loaded');
+}
